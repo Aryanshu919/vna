a7708a3793943bea712b26415bc57bf301d18a76
Aryan Kumar <aryanshu919@gmail.com>
2025-01-25 03:26:46 +0530

    added acertinity

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import { ConnectSection } from './components/ConnectSection';
 import { Footerr } from './components/Footerr';
+import {InfiniteMovingCards} from ""
 
 
 function App() {
   return (
     <div className="bg-slate-500">
+      
         <ConnectSection/>
         <Footerr/>
     </div>
